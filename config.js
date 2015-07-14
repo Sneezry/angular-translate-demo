ac9a93d3c34586505a8141763e9b55801e9caae9
lizhe <lizhe@smartisan.com>
2015-07-14 17:19:40 +0800

    init

---
+++ config.js
@@ -0,0 +1,9 @@
+﻿var app = angular.module('i18n', ['pascalprecht.translate']);
+
+app.config(function($translateProvider) {
+  $translateProvider.useStaticFilesLoader({
+    prefix: '/',
+    suffix: '.json'
+  });
+  $translateProvider.preferredLanguage(localStorage.lang || 'zh-CN');
+});
---
+++ controller.js
@@ -0,0 +1,15 @@
+﻿app.controller('translate', function($translate, $scope) {
+  $scope.initLang = function() {
+    $scope.lang = localStorage.lang || 'zh-CN';
+  }
+
+  $scope.isAvailableItem = function(item) {
+    return !/^i18n\./.test(item);
+  }
+
+  $scope.setLanguage = function(lang) {
+    $translate.use(lang);
+    $scope.lang = lang;
+    localStorage.lang = lang;
+  }
+});
