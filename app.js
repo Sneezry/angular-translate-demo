52ed1429dd305c3c61d2c1f33c76f601fc279808
lizhe <lizhe@smartisan.com>
2015-07-14 18:43:26 +0800

    闭包

--- app.js
+++ app.js
@@ -8,20 +8,4 @@
     });
     $translateProvider.preferredLanguage(localStorage.lang || 'zh-CN');
   });
-
-  app.controller('translate', function($translate, $scope) {
-    $scope.initLang = function() {
-      $scope.lang = localStorage.lang || 'zh-CN';
-    }
-
-    $scope.isAvailableItem = function(item) {
-      return !/^i18n\./.test(item);
-    }
-
-    $scope.setLanguage = function(lang) {
-      $translate.use(lang);
-      $scope.lang = lang;
-      localStorage.lang = lang;
-    }
-  });
 })();
---
+++ controller.js
@@ -0,0 +1,19 @@
+(function(){
+  var app = angular.module('i18n');
+
+  app.controller('translate', function($translate, $scope) {
+    $scope.initLang = function() {
+      $scope.lang = localStorage.lang || 'zh-CN';
+    }
+
+    $scope.isAvailableItem = function(item) {
+      return !/^i18n\./.test(item);
+    }
+
+    $scope.setLanguage = function(lang) {
+      $translate.use(lang);
+      $scope.lang = lang;
+      localStorage.lang = lang;
+    }
+  });
+})();
